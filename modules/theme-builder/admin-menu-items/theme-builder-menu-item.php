2898043013e08223f806b0336889d88b4b87aca9
Luca Castellone <luca.castellone@gmail.com>
2022-10-14 14:41:20 +0200

    v3.7.7

--- modules/theme-builder/admin-menu-items/theme-builder-menu-item.php
+++ modules/theme-builder/admin-menu-items/theme-builder-menu-item.php
@@ -1,32 +1,31 @@
 <?php
-
-namespace ElementorPro\Modules\Popup\AdminMenuItems;
+namespace ElementorPro\Modules\ThemeBuilder\AdminMenuItems;
 
 use Elementor\Core\Admin\Menu\Interfaces\Admin_Menu_Item;
 use Elementor\TemplateLibrary\Source_Local;
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit; // Exit if accessed directly.
 }
 
-class Popups_Menu_Item implements Admin_Menu_Item {
+class Theme_Builder_Menu_Item implements Admin_Menu_Item {
 	public function get_capability() {
 		return 'publish_posts';
 	}
 
 	public function get_label() {
-		return esc_html__( 'Popups', 'elementor-pro' );
+		return esc_html__( 'Theme Builder', 'elementor-pro' );
 	}
 
 	public function get_parent_slug() {
 		return Source_Local::ADMIN_MENU_SLUG;
 	}
 
-	public function get_position() {
-		return null;
-	}
-
 	public function is_visible() {
 		return true;
+	}
+
+	public function get_position() {
+		return null;
 	}
 }
